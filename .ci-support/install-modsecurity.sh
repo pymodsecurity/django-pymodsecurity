bf1f8dd09a51cab51ae779a8f22c177adc7e30d4
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-10-08 23:19:43 -0300

    Add travis config

---
+++ .ci-support/install-modsecurity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y g++ flex bison curl doxygen libyajl-dev libgeoip-dev libtool dh-autoreconf libcurl4-gnutls-dev libxml2 libpcre++-dev libxml2-dev git cmake libtool automake autoconf m4 pkg-config || exit 1
+
+wget -O modsecurity.tar.gz https://github.com/SpiderLabs/ModSecurity/releases/download/v$MODSECURITY_VERSION/modsecurity-v$MODSECURITY_VERSION.tar.gz || exit 1
+
+tar xvf modsecurity.tar.gz || exit 1
+
+pushd modsecurity-v$MODSECURITY_VERSION || exit 1
+
+./configure --prefix=/usr && make -j 2 && sudo make install && sudo ldconfig || exit 1
+
+popd || exit 1
